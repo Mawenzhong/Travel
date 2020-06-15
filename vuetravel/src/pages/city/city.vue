872fae0440251b2a0cf669a81ac9d363110b28a2
Procedure <1962569733@qq.com>
2020-06-15 16:22:58 +0800

    add search

--- vuetravel/src/pages/city/city.vue
+++ vuetravel/src/pages/city/city.vue
@@ -1,12 +1,17 @@
 <template>
+<div>
 <cityheader></cityheader>
+<citysearch></citysearch>
+</div>
 </template>
 <script>
 import cityheader from './components/cityheader'
+import citysearch from './components/search'
 export default {
   name: 'city',
   components: {
-    cityheader
+    cityheader,
+    citysearch
   }
 }
 </script>
---
+++ vuetravel/src/pages/city/components/search.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+<script>
+export default {
+
+  name: 'citysearch'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varbless.styl'
+.search
+  height:.72rem
+  background:$bgcolor
+  padding:0 .1rem
+.search-input
+  box-sizing:border-box
+  padding:0 .1rem
+  height:.6rem
+  width:100%
+  text-align:center
+</style>
